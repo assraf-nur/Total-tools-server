4a1456dc91c940e42cde1a46d47064f16d716cd7
assraf-nur <assraf.nur@gmail.com>
2022-05-23 00:12:36 +0600

    add JWT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -10,33 +11,41 @@ app.use(cors());
 app.use(express.json());
 
 // connect mongo
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wu6pcws.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wu6pcws.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
+// apply JWT
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decode) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decode;
+    next();
+  });
+}
+
 async function run() {
   try {
-
     await client.connect();
-    const toolsCollection = client.db('total-tools').collection('tools');
-      
+    const toolsCollection = client.db("totalTools").collection("tools");
 
-    app.get('/tools',async (req, res) =>{
-        const query = {};
-        const cursor = toolsCollection.find(query);
-        const tools = await cursor.toArray();
-        res.send(tools);
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const cursor = toolsCollection.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
     });
-
-
-  } 
-  
-  finally {
-
+  } finally {
   }
 }
 run().catch(console.dir);

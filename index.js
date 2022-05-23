1121a00c1dd8778a29aa0288ec43a4e5c5c6cc2f
assraf-nur <assraf.nur@gmail.com>
2022-05-24 00:06:00 +0600

    add api

--- index.js
+++ index.js
@@ -68,6 +68,10 @@ async function run() {
       res.send(singleOrders);
     })
 
+    app.get('/users', async(req, res) =>{
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    })
     
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;

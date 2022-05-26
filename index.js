5b1bd9816178cf54bcefbd9068f5b74b68238b88
assraf-nur <assraf.nur@gmail.com>
2022-05-26 12:27:49 +0600

    add delete api

--- index.js
+++ index.js
@@ -174,6 +174,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
     app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
       const user = await usersCollection.findOne({ email: email });
